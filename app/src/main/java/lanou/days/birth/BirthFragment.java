baa547c1796d9b50a630b74c7345f2a96561a5d1
马闯 <674195845@qq.com>
2016-11-22 17:01:25 +0800

    no message

--- app/src/main/java/lanou/days/birth/BirthFragment.java
+++ app/src/main/java/lanou/days/birth/BirthFragment.java
@@ -1,23 +1,42 @@
 package lanou.days.birth;
 
+import android.content.Intent;
+
+import android.view.View;
+import android.widget.Button;
+
+
 import lanou.days.base.BaseFragment;
 
 /**
- * Created by dllo on 16/11/22.
+ * Created by machuang on 16/11/22.
  */
-public class BirthFragment extends BaseFragment {
+public class BirthFragment extends BaseFragment implements View.OnClickListener {
+
+    private Button birthModify;
+
     @Override
     protected void initDate() {
-
+        setItemOnClick(this,birthModify);
     }
 
     @Override
     protected void initView() {
-
+        birthModify = bindView(R.id.btn_birth_modify);
     }
 
     @Override
     protected int getLayout() {
-        return 0;
+        return R.layout.fragment_birth;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_birth_modify:
+                Intent intent = new Intent(getContext(),ModifyBirthDayActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/lanou/days/birth/ModifyBirthDayActivity.java
@@ -0,0 +1,24 @@
+package lanou.days.birth;
+
+import lanou.days.R;
+import lanou.days.base.BaseActivity;
+
+/**
+ * Created by dllo on 16/11/22.
+ */
+public class ModifyBirthDayActivity extends BaseActivity {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_modify;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+}
