beb029c92e8bd2185a83518410fe73393cb4f66f
贾志强 <70686114@qq.com>
2016-11-23 15:57:15 +0800

    完成了侧滑菜单的大概框架

--- app/src/main/java/lanou/days/MainActivity.java
+++ app/src/main/java/lanou/days/MainActivity.java
@@ -1,7 +1,14 @@
 package lanou.days;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.RadioButton;
@@ -12,14 +19,14 @@ import lanou.days.note.NoteFragment;
 import lanou.days.setting.SettingFragment;
 import lanou.days.write.WriteFragment;
 
-public class MainActivity extends BaseActivity implements View.OnClickListener {
+public class MainActivity extends BaseActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
     private RadioButton btnWrite,btnNote,btnBirth,btnSetting;
     private FragmentManager manager;
     private FragmentTransaction transaction;
     @Override
     protected int getLayout() {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        return R.layout.activity_main;
+        return R.layout.activity_drawer;
     }
 
     @Override
@@ -32,6 +39,9 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void initData() {
+
+        initDrawerLayout();
+
         btnWrite.setChecked(true);
         manager = getSupportFragmentManager();
         transaction = manager.beginTransaction();
@@ -46,6 +56,19 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
 
     }
+    //创建侧滑
+    private void initDrawerLayout() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.dl);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.main_nv);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
     @Override
     public void onClick(View view) {
@@ -58,7 +81,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
             case R.id.btn_main_note:
                 transaction.replace(R.id.lb_main, new NoteFragment());
                 break;
-
             case R.id.btn_main_birth:
                 transaction.replace(R.id.lb_main, new BirthFragment());
                 break;
@@ -68,5 +90,14 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         }
 
 
+    }
+
+    //侧滑中item的点击
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.dl);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
