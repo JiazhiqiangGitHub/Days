804eae0967672d7648ac2b18dffe1764377841cc
贾志强 <70686114@qq.com>
2016-11-24 16:46:48 +0800

    完善了登录

--- app/src/main/java/lanou/days/news/NewsFragment.java
+++ app/src/main/java/lanou/days/news/NewsFragment.java
@@ -1,5 +1,11 @@
 package lanou.days.news;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+
 import lanou.days.R;
 import lanou.days.base.BaseFragment;
 
@@ -7,14 +13,26 @@ import lanou.days.base.BaseFragment;
  * Created by dllo on 16/11/23.
  */
 public class NewsFragment extends BaseFragment {
+    private TabLayout tb;
+    private TabAdapter adapter;
+    private ViewPager vp;
+    private ArrayList<Fragment> fragment;
     @Override
     protected void initDate() {
-        
+
+        adapter = new TabAdapter(getChildFragmentManager());
+        fragment = new ArrayList<>();
+        fragment.add(new ReadFragment());
+        fragment.add(new TelchnologyFragment());
+        adapter.setFragments(fragment);
+        vp.setAdapter(adapter);
+        tb.setupWithViewPager(vp);
     }
 
     @Override
     protected void initView() {
-
+        vp = bindView(R.id.vp_news);
+        tb = bindView(R.id.tb_news);
     }
 
     @Override
