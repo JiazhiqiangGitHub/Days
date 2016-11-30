0df9ff6cabba0994cdc26be98e587163cdc9252a
陈丰尧 <734481463@qq.com>
2016-11-30 10:25:05 +0800

    小改动

--- app/src/main/java/lanou/days/values/Values.java
+++ app/src/main/java/lanou/days/values/Values.java
@@ -4,7 +4,7 @@ package lanou.days.values;
  * Created by 贾志强 on 16/11/28.
  */
 
-public class Values {
+public final class Values {
     public static final String URL_READTWO = "http://api.liwushuo.com/v2/channels/108/items_v2?ad=2&gender=1&generation=1&limit=20&offset=";
     public static final String URL_READONE = "http://api.liwushuo.com/v2/channels/108/items_v2?ad=2&gender=1&generation=1&limit=20&offset=0";
     public static final String URL_READHEAD = "http://api.liwushuo.com/v2/banners";
